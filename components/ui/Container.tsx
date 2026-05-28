e944b3e7abc1667aaf3ea5d5e69e132005ece297
SkSin19 <sakshamsinha9999@gmail.com>
2026-05-28 22:07:19 +0530

    error fixed

--- components/ui/Container.tsx
+++ components/ui/Container.tsx
@@ -1,19 +1,21 @@
+import * as React from "react";
+
 interface ContainerProps {
   children: React.ReactNode;
   className?: string;
   as?: React.ElementType;
   id?: string;
 }
-
 export default function Container({
   children,
   className = "",
   as: Component = "div",
   id,
 }: ContainerProps) {
-  return (
-    <Component id={id} className={`container-main ${className}`}>
-      {children}
-    </Component>
+  return React.createElement(
+    // Use React.createElement to avoid JSX typing issues when `Component` is dynamic
+    Component as any,
+    { id: id as any, className: `container-main ${className}` } as any,
+    children
   );
 }
